9b7c8850d5ec19109ac945364aa56eb08a16c643
Yara-Afaneh <yara.afaneh@gmail.com>
2023-12-03 16:02:57 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,150 @@
+var courseName=document.querySelector("#courseName");
+var courseCategory=document.querySelector("#courseCategory");
+var coursePrice=document.querySelector("#coursePrice");
+var courseDescription=document.querySelector("#courseDescription");
+var courseCapacity=document.querySelector("#courseCapacity");
+var addbtn=document.querySelector("#click");
+var search=document.querySelector("#search");
+var nameerror=document.querySelector(".nameerror");
+var isNameTrue=true;
+
+var courses=[];
+var inputs=document.querySelectorAll(".inputs")
+
+addbtn.addEventListener("click",function(e){
+    e.preventDefault();
+     addCourse();
+     displaydata();
+     clearinputs();
+})
+ 
+function addCourse(){
+    var course={
+        name:courseName.value,
+        category:courseCategory.value,
+        price:coursePrice.value,
+        description:courseDescription.value,
+        capacity:courseCapacity.value,
+    }
+    courses.push(course);
+
+  
+   
+}
+
+function clearinputs() {
+    for (var i = 0; i < inputs.length; i++) {
+        inputs[i].value ="";
+    }
+}
+function displaydata() {
+    var result='';
+    for(var i=0; i<courses.length;i++){
+        result+=`
+        <tr>
+            <td>${i}</td>
+            <td>${courses[i].name}</td>
+            <td>${courses[i].category}</td>
+            <td>${courses[i].price}</td>
+            <td>${courses[i].description}</td>
+            <td>${courses[i].capacity}</td>
+            <td><button class="btn btn-outline-info">update</button></td>
+            <td><button class="btn btn-outline-danger" onclick="deleteCourse(${i})">delete</button></td>
+        </tr>
+        `;
+    }
+    
+    document.getElementById("data").innerHTML=result;
+}
+
+function deleteCourse(id){
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+          confirmButton: 'btn btn-success',
+          cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+      })
+      
+      swalWithBootstrapButtons.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Yes, delete it!',
+        cancelButtonText: 'No, cancel!',
+        reverseButtons: true
+      }).then((result) => {
+        if (result.isConfirmed) {
+            courses.splice(id, 1);
+            displaydata();
+          swalWithBootstrapButtons.fire(
+            'Deleted!',
+            'Your file has been deleted.',
+            'success'
+          )
+        } else if (
+          /* Read more about handling dismissals below */
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          swalWithBootstrapButtons.fire(
+            'Cancelled',
+            'Your imaginary file is safe :)',
+            'error'
+          )
+        }
+      })
+   
+}
+
+search.addEventListener("keyup",function(e){
+    var result='';
+    for(var i=0; i<courses.length;i++){
+        if(courses[i].name.toLowerCase().includes(e.target.value.toLowerCase()))
+        result+=`
+        <tr>
+            <td>${i}</td>
+            <td>${courses[i].name}</td>
+            <td>${courses[i].category}</td>
+            <td>${courses[i].price}</td>
+            <td>${courses[i].description}</td>
+            <td>${courses[i].capacity}</td>
+            <td><button class="btn btn-outline-info">update</button></td>
+            <td><button class="btn btn-outline-danger" onclick="deleteCourse(${i})">delete</button></td>
+        </tr>
+        `;
+    }
+    document.getElementById("data").innerHTML=result;
+})
+
+courseName.addEventListener("keyup", function(){
+    var pattern =/^[A-Z][a-z]{2,10}$/;
+      if(pattern.test(courseName.value)){
+        if(courseName.classList.contains("is-invalid")){
+           courseName.classList.remove("is-invalid");
+           courseName.classList.add("is-valid");
+        }
+
+        courseName.classList.add("is-valid");
+        nameerror.style.csstext="display:none";
+        isNameTrue=true;
+    }else{
+        if(courseName.classList.contains("is-valid")){
+        courseName.classList.remove("is-valid");
+        courseName.classList.add("is-invalid");
+     }
+
+     courseName.classList.add("is-invalid");
+     nameerror.style.csstext="display:block";
+     isNameTrue=false;
+
+    }
+
+   if (isNameTrue){
+    addbtn.removeAttribute('disabled')
+   }else{
+    addbtn.setAttribute('disabled', 'disabled')
+   }
+
+});
+
